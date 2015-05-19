9ae915e162fc40aee84e2d003920ad7d0b741dbc
Romeu Fonseca <romeu.hcf@gmail.com>
2015-05-19 14:38:16 -0300

    Fixing runner monitor call

--- app/workers/docker_runner_monitor_worker.rb
+++ app/workers/docker_runner_monitor_worker.rb
@@ -1,13 +1,13 @@
 class DockerRunnerMonitorWorker
   include Sidekiq::Worker
-  def perform(repo_id, build_job_id)
+  def perform(build_job_id)
     build_job = BuildJob.find(build_job_id)
-
     runner = DockerRunner.from_container_id(build_job.container_id)
     build_job.log = runner.logs
 
     if runner.running?
-      self.class.perform_in(5, repo_id, revision, runner.id)
+      self.class.perform_in(5, build_job_id)
+
     else
       build_job.exit_status = runner.exit_status
       build_job.finished_at = runner.finished_at
